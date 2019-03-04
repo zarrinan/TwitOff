e27433c40726ec41401e3e5c63756ef806d5f7a0
Zarrina <zarrina_@hotmail.com>
2019-03-04 14:48:43 -0500

    Connect SQLAlchemy DB to app

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,10 +1,13 @@
-# main app and routing logic
+"""Main application and routing logic for TwitOff."""
 from flask import Flask
+from .models import DB
 
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    DB.init_app(app)
 
     @app.route('/')
     def root():
