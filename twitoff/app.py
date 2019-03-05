a32bcd1cde6d0b916b3e99739556ba498539e752
Zarrina <zarrina_@hotmail.com>
2019-03-05 13:08:59 -0500

    Add tweepy, basilica, python-decouple, html template

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,16 +1,19 @@
 """Main application and routing logic for TwitOff."""
-from flask import Flask
-from .models import DB
+from flask import Flask,render_template,request
+from .models import DB, User
 
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['ENV'] = 'debug'
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return 'Welcome to TwitOff!'
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
 
     return app
