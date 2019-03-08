76c9b4da65fc330aff17be4356735ca38897c212
Zarrina <zarrina_@hotmail.com>
2019-03-07 20:27:27 -0800

    Add predict, second user form, compare route and controller

--- twitoff/app.py
+++ twitoff/app.py
@@ -2,6 +2,7 @@
 from decouple import config
 from flask import Flask,render_template,request
 from .models import DB, User
+from .predict import predict_user
 from .twitter import add_or_update_user
 
 
@@ -19,25 +20,33 @@ def create_app():
 
     @app.route('/user', methods=['POST'])
     @app.route('/user/<name>', methods=['GET'])
-    def user(name=None, message=""):
-        # import pdb; pdb.set_trace()
+    def user(name=None):
+        message = ''
         name = name or request.values['user_name']
         try:
             if request.method == 'POST':
                 add_or_update_user(name)
-                message = "User {} successfully added!".format(name)
+                message = 'User {} successfully added!'.format(name)
             tweets = User.query.filter(User.name == name).one().tweets
         except Exception as e:
-            message = "Error addind {}: {}".format(name, e)
+            message = 'Error adding {}: {}'.format(name, e)
             tweets = []
         return render_template('user.html', title=name, tweets=tweets,
-                                message=message)
+                               message=message)
 
-
+    @app.route('/compare', methods=['POST'])
+    def compare():
+        user1, user2 = request.values['user1'], request.values['user2']
+        if user1 == user2:
+            return 'Cannot compare a user to themselves!'
+        else:
+            prediction = predict_user(user1, user2,
+                                      request.values['tweet_text'])
+            return user1 if prediction else user2
 
     @app.route('/reset')
     def reset():
         DB.drop_all()
         DB.create_all()
-        return render_template('base.html', title='DB reset', users=[])
+        return render_template('base.html', title='DB Reset!', users=[])
     return app
--- twitoff/twitter.py
+++ twitoff/twitter.py
@@ -14,25 +14,26 @@ BASILICA = basilica.Connection(config('BASILICA_KEY'))
 
 #TODO: some useful methods
 def add_or_update_user(username):
-    """Add or update the user or their tweets"""
+    """Add or update a user *and* their Tweets, error if no/private user."""
     try:
-        tweeter_user = TWITTER.get_user(username)
-        db_user = (User.query.get(twiter_user.id) or
-                   User(id = twitter_user.id, name=username))
+        twitter_user = TWITTER.get_user(username)
+        db_user = (User.query.get(twitter_user.id) or
+                   User(id=twitter_user.id, name=username))
         DB.session.add(db_user)
+        # We want as many recent non-retweet/reply statuses as we can get
         tweets = twitter_user.timeline(
             count=200, exclude_replies=True, include_rts=False,
-            tweet_mode='extended', since_id = db_user.newest_tweet_id)
+            tweet_mode='extended', since_id=db_user.newest_tweet_id)
         if tweets:
-            db_user.newest_tweet_id = tweet[0].id
+            db_user.newest_tweet_id = tweets[0].id
         for tweet in tweets:
-            #get embedding for tweet, and store in db
+            # Get embedding for tweet, and store in db
             embedding = BASILICA.embed_sentence(tweet.full_text,
-                                                mode="twitter")
-            db_tweet = Tweet(id = tweet.id, tweet=tweet.full_text[:500],
-                             embedding = embedding)
+                                                model='twitter')
+            db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:500],
+                             embedding=embedding)
             db_user.tweets.append(db_tweet)
-            B.session.add(db_tweet)
+            DB.session.add(db_tweet)
     except Exception as e:
         print('Error processing {}: {}'.format(username, e))
         raise e
